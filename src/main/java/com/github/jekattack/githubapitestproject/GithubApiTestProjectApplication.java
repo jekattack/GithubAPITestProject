8b377e175d02d41fc650e2c8f32d6090a56377cc
Jan Kraus <jan.erik.kraus@t-online.de>
2022-06-01 16:07:08 +0200

    init

---
+++ src/main/java/com/github/jekattack/githubapitestproject/GithubApiTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.jekattack.githubapitestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/jekattack/githubapitestproject/GithubApiTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jekattack.githubapitestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
